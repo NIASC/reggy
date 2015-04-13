0450dfa1a178eebac8f14d680ad09eb5d40e26a1
Sigurd Gartmann <sigurd.gartmann@ntnu.no>
2015-04-13 14:37:17 +0200

    Add registry server where registry owners accept or decline queries
    This should run on registries own hardware, and have customized data
    fetching.

--- README.md
+++ README.md
@@ -6,15 +6,18 @@ Biobank inventory
 
 CSV files contain dummy data
 
-``merge.py`` runs a simple merge server on http://localhost:5000, that will merge data coming from different registries.
+``merge.py`` runs a simple merge server on http://localhost:5000, that will
+merge data coming from different registries.
 
-``summarize.py`` is as service to do simple filtering on the results.
+``summarize.py`` is as service to do simple filtering (aka counting) on the
+results.
 
 ``query.py`` will serve the registries new queries on http://localhost:5001.
 
 ``web.py`` will serve users a form where they can create queries
 
-``reg.py [cancer|hunt]`` will fetch queries from _query_ servers and push fake data to the _merge_ server. Run for both fake registrys to get data to be merged. Merged results now show up in merge.py output. Will later be pushed to a result/filter/report/summary layer or two.
+``registry.py`` will fetch queries from _query_ servers and push fake data to
+the _merge_ server, when registry owners accept the queries.
 
 Dependencies
 ------------
@@ -39,10 +42,10 @@ Usage
 -----
 
 In a lot of terminals, run ``source venv/bin/activate`` and one of the
-services: web, query, merge, result. Then do a few queries from
-``localhost:5000/search`` and run ``reg.py hunt`` and ``reg.py cancer``. There
-should hopefully be some status output in the terminal running the summarize
-command.
+services: web, query, merge, summarize, registry. Then do a few queries from
+``localhost:5000/search`` and do manual filtering for the registries at
+``localhost:5005``. There should hopefully be some status output in the
+terminal running the summarize command.
 
 Configuration
 -------------
--- registry.py
+++ registry.py
@@ -1,28 +1,27 @@
 #!/usr/bin/env python2
 
 """
-This will mimic a health registry, not using correct data, but something similar.
+This will mimic a health registry, not using correct data, but something
+similar.
 """
 
 import os
-import sys
 import json
 import urllib2
 import csv
-import logging
 import hashlib
 import gnupg
+from flask import Flask, render_template, redirect
 
-source_id = sys.argv[1]
+app = Flask(__name__)
 
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger(source_id)
+configfile = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                          'config.json')
 
-configfile = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config.json')
 try:
     config = json.load(open(configfile))
 except IOError:
-    logger.info('Config file not found, using defaults')
+    app.logger.info('Config file not found, using defaults')
     config = {}
 
 # get encryption config from config file
@@ -37,6 +36,7 @@ hash_config = config.get('hashing', {})
 gpg = gnupg.GPG(gnupghome=keydir)
 gpg.encoding = 'utf-8'
 
+
 def find_indexes_from_fieldnames(headers, fieldnames):
     indexes = []
     for fieldname in fieldnames:
@@ -47,17 +47,19 @@ def find_indexes_from_fieldnames(headers, fieldnames):
             pass
     return indexes
 
+
 def hash_id(registry_person_id):
     hash = hashlib.sha256()
     hash.update(hash_config.get('salt', "default salt should be overwritten"))
     hash.update(registry_person_id)
     return hash.hexdigest()
 
-def get_local_data(fieldnames):
+
+def get_local_data(fieldnames, source_id):
     with open(source_id + '.csv', 'r') as f:
         reader = csv.reader(f)
         tabular_data = [row for row in reader]
-        headers = [ h.strip() for h in tabular_data[0] ]
+        headers = [h.strip() for h in tabular_data[0]]
         indexes_to_use = find_indexes_from_fieldnames(headers, fieldnames)
         data = {}
         for line in tabular_data[1:]:
@@ -69,32 +71,67 @@ def get_local_data(fieldnames):
             for field in indexes_to_use:
                 obj[headers[field]] = line[field]
             if encryption_config.get('encrypt_data', True):
-                recipient_email = encryption_config.get('recipient', 'sigurdga@edge')
+                recipient_email = encryption_config.get('recipient',
+                                                        'sigurdga@edge')
                 obj = gpg.encrypt(json.dumps(obj), [recipient_email]).data
             data[id] = obj
         return {'data': data, 'source_id': source_id}
 
+
 def send_data(data):
-    logger.debug("sending: %s", data)
+    app.logger.debug("sending: %s", data)
     try:
         urllib2.urlopen('http://localhost:5002/', json.dumps(data))
     except urllib2.HTTPError, e:
-        logger.error(e)
+        app.logger.error(e)
+
 
 def fetch_queries(source_id):
-    logger.debug("fetching queries")
+    app.logger.debug("fetching queries")
     try:
         res = urllib2.urlopen('http://localhost:5001/' + source_id)
         return res.read()
     except urllib2.HTTPError, e:
-        logger.error(e)
+        app.logger.error(e)
 
-queries = fetch_queries(source_id)
-if queries:
-    decoded_queries = json.loads(queries)
-    for query in decoded_queries['queries']:
-        fieldnames = query['fields']
-        data = get_local_data(fieldnames)
-        data['query_id'] = query['id']
-        data['sources'] = query['sources']
-        send_data(data)
+
+@app.route("/")
+def index():
+    return render_template('registries.html')
+
+
+@app.route("/<source_id>")
+def queries(source_id):
+    queries = fetch_queries(source_id)
+    if queries:
+        decoded_queries = json.loads(queries)
+        return render_template('queries.html',
+                               queries=decoded_queries['queries'],
+                               source_id=source_id)
+
+
+@app.route("/<source_id>/<method>/<query_id>")
+def send(source_id, method, query_id):
+    queries = fetch_queries(source_id)
+    if queries:
+        decoded_queries = json.loads(queries)
+        for query in decoded_queries["queries"]:
+            print query
+            if query["id"] == query_id:
+                if method == "accept":
+                    fieldnames = query['fields']
+                    data = get_local_data(fieldnames, source_id)
+                    data['query_id'] = query['id']
+                    data['sources'] = query['sources']
+                    send_data(data)
+                    return redirect("/"+source_id)
+                else:
+                    data = {}
+                    data['query_id'] = query['id']
+                    data['sources'] = query['sources']
+                    send_data(data)
+                    return redirect("/"+source_id)
+
+
+if __name__ == '__main__':
+    app.run(port=5005, debug=True)
