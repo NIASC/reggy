d1806ec76632537564cc98882205534058797a14
Sigurd Gartmann <sigurd.gartmann@ntnu.no>
2015-08-07 12:28:21 +0200

    Add some unittests for merging

---
+++ tests/test_merge.py
@@ -0,0 +1,26 @@
+import unittest
+
+from merge import merge
+
+
+class TestMerge(unittest.TestCase):
+
+    def test_merge_different_ids_should_give_empty_results(self):
+        data = {'reg1': {'id1': 'encrypted_data1'},
+                'reg2': {'id2': 'encrypted_data2'}}
+        merged = merge(data)
+
+        self.assertEqual(merged, [])
+
+    def test_merge_same_id_should_give_merged_result(self):
+        data = {'reg1': {'id1': 'encrypted_data1'},
+                'reg2': {'id1': 'encrypted_data2'}}
+        merged = merge(data)
+
+        # convert first and only line to a set for easy comparing in test
+        self.assertEqual(set(merged[0]),
+                         set(('encrypted_data1', 'encrypted_data2')))
+
+
+if __name__ == '__main__':
+    unittest.main()
