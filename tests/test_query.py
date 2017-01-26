19de575ef75b7b22054630939aecbc49a4a93ad5
Sigurd Gartmann <sigurd.gartmann@ntnu.no>
2017-01-26 12:46:33 +0100

    Update old test after not recent changes elsewhere

--- tests/test_query.py
+++ tests/test_query.py
@@ -1,14 +1,14 @@
 import unittest
-from query import filter_queries, cached_queries
+from query import filter_queries, signed_query_store
 
 
 class TestQuery(unittest.TestCase):
 
     def test_filter_queries(self):
-        cached_queries['query_id_1'] = {'sources': ['reg1'], 'query_id': 'q1'}
-        cached_queries['query_id_2'] = {'sources': ['reg2'], 'query_id': 'q2'}
+        signed_query_store['query_id_1'] = {'sources': ['reg1'], 'query_id': 'q1'}
+        signed_query_store['query_id_2'] = {'sources': ['reg2'], 'query_id': 'q2'}
         filtered = filter_queries('reg1')
-        assert filtered['queries'][0]['query_id'] == 'q1'
+        assert filtered[0]['query_id'] == 'q1'
         assert len(filtered) == 1
 
 
