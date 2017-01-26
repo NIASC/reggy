1ce16655b4e14093742c2dc1268a9d660d3d0819
Sigurd Gartmann <sigurd.gartmann@ntnu.no>
2017-01-26 13:04:59 +0100

    Put filters inside Filter class

--- registry_filter.py
+++ registry_filter.py
@@ -1,21 +1,17 @@
 #!/usr/bin/env python3
 
 """
-Filter parsing and filtering
+Filter parsing and filtering.
+
+The rootlevel filters are where we say dataowner A may be combined with dataowner B.
+The leaflevel filters are where we say field A1 may be combined with field B2 (at different dataowners).
+
+Filters are checked both ways.
 """
 
-filters = {
-        'rootlevel': {
-            'hunt': {
-                'cancer': True
-                },
-            'cancer': {
-                'hunt': True
-                },
-            },
-        }
-
 class Filter(object):
+    def __init__(self, filters):
+        self.filters = filters
 
     def filter(self, query):
         for dataowner, fields in query.items():
@@ -24,8 +20,14 @@ class Filter(object):
             for other_dataowner, other_fields in query.items():
                 if dataowner == other_dataowner:
                     continue
-                side_a = filters['rootlevel'][dataowner][other_dataowner]
-                side_b = filters['rootlevel'][other_dataowner][dataowner]
+                try:
+                    side_a = self.filters['rootlevel'][dataowner][other_dataowner]
+                except KeyError:
+                    return False
+                try:
+                    side_b = self.filters['rootlevel'][other_dataowner][dataowner]
+                except KeyError:
+                    return False
                 if side_a and side_b:
                     continue
                 else:
@@ -34,10 +36,22 @@ class Filter(object):
 
 
 if __name__ == '__main__':
+    # Just an example. Will be removed. TODO
     query = {
         'hunt': ['age', 'gender'],
         'cancer': ['lung_cancer'],
         }
-    f = Filter('hunt')
+    filters = {
+            'rootlevel': {
+                'hunt': {
+                    'cancer': True
+                    },
+                'cancer': {
+                    'hunt': True
+                    },
+                },
+            }
+
+    f = Filter(filters)
     status = f.filter(query)
-    print(status, "ST")
+    print(status)
--- tests/test_registry_filter.py
+++ tests/test_registry_filter.py
@@ -2,28 +2,34 @@ import unittest
 
 from registry_filter import Filter
 
-filters = {
-        'rootlevel': {
-            'hunt': {
-                'cancer': True
-                },
-            'cancer': {
-                'hunt': True
-                },
-            },
-        }
-
-query = {
-    'hunt': ['age', 'gender'],
-    'cancer': ['lung_cancer'],
-    }
 
 class TestFilter(unittest.TestCase):
     def setUp(self):
-        self.f = Filter()
+        filters = {
+                'rootlevel': {
+                    'hunt': {
+                        'cancer': True
+                        },
+                    'cancer': {
+                        'hunt': True
+                        },
+                    },
+                }
+        self.f = Filter(filters)
 
     def test_rootlevel_both_ways(self):
+        query = {
+                'hunt': ['age', 'gender'],
+                'cancer': ['lung_cancer'],
+                }
         self.assertTrue(self.f.filter(query))
+
+    def test_rootlevel_unknown_dataowner(self):
+        query = {
+                'hunt': ['age', 'gender'],
+                'bad': [''],
+                }
+        self.assertFalse(self.f.filter(query))
 
 if __name__ == '__main__':
     unittest.main()
